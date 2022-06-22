d64c9733fb793c2563d5a3d2224e601ab5f8f7fd
Vladislav Aparin <34743581+stinex@users.noreply.github.com>
2022-06-22 09:23:26 +0500

    fix

--- src/components/ShowRooms/ShowRoomsMap.js
+++ src/components/ShowRooms/ShowRoomsMap.js
@@ -4,11 +4,7 @@ const ShowRoomsMap = () => {
     return (
         <section className='showrooms-map'>
             <div className='showrooms-map__wrapper'>
-                <img
-                    className='showrooms-map__img'
-                    src='/image/ShowRooms/map.jpg'
-                    alt='map'
-                />
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A46af03b653282dad9f2fcd1807efa80aea8152fa7fb98d1816fd87f500150845&amp;source=constructor" width="100%" height="100%" frameborder="0"></iframe>
             </div>
         </section>
     );
